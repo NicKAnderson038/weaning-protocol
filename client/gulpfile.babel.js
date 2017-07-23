30ab7b27c25172ca2a153339dbe57516ba2f86a6
NicKAnderson038 <nickanderson@gmail.com>
2017-07-23 16:52:51 -0700

    gulp environment varibles set dev & prod

--- client/gulpfile.babel.js
+++ client/gulpfile.babel.js
@@ -8,7 +8,9 @@ import cleanCSS from 'gulp-clean-css'
 import sourcemaps from 'gulp-sourcemaps'
 import htmlmin from 'gulp-htmlmin'
 import imagemin from 'gulp-imagemin'
+import gulpIf from 'gulp-if'
 
+let env = process.env.NODE_ENV || 'development'
 
 const gulpTasks = [
   'transpile',
@@ -21,17 +23,17 @@ const gulpTasks = [
 ]
 
 const css = [
-    "src/css/angular-toastr.css",
-    "src/css/bootstrap.css",
-    "src/css/font-awesome.min.css",
-    "src/css/hoverex-all.css",
-    "src/css/prettyPhoto.css",
-    "src/css/style.css",
+  "src/css/angular-toastr.css",
+  "src/css/bootstrap.css",
+  "src/css/font-awesome.min.css",
+  "src/css/hoverex-all.css",
+  "src/css/prettyPhoto.css",
+  "src/css/style.css",
 ]
 
 const lib = [
-    'src/lib/jquery.min.js',
-    'src/lib/bootstrap.min.js'
+  'src/lib/jquery.min.js',
+  'src/lib/bootstrap.min.js'
 ]
 
 gulp.task("default", gulpTasks)
@@ -51,6 +53,9 @@ gulp.task("transpile", () => {
     // .pipe(uglify({
     //   mangle:false
     // }))
+    .pipe(gulpIf(env === 'production', uglify({
+      mangle: false
+    })))
     .pipe(gulp.dest("dist"))
 
 })
@@ -70,52 +75,52 @@ gulp.task('minify-concat-css', () => {
 
 /* HTML */
 gulp.task('indexHTML', () => {
-    return gulp.src(['index.html'])
-      .pipe(htmlmin({
-        collapseWhitespace: true,
-        minifyJS: true,
-        minifyCSS: true,
-        removeComments: true
-      }))
-      .pipe(gulp.dest('dist'))
+  return gulp.src(['index.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCSS: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('minifyHTML', () => {
-    return gulp.src(['views/*.html'])
-      .pipe(htmlmin({
-        ignoreCustomFragments:[ /<|>/ ],
-        collapseWhitespace: true,
-        minifyJS: true,
-        minifyCSS: true,
-        removeComments: true
-        
-      }))
-      .pipe(gulp.dest('dist/views'))
+  return gulp.src(['views/*.html'])
+    .pipe(htmlmin({
+      ignoreCustomFragments: [/<|>/],
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCSS: true,
+      removeComments: true
+
+    }))
+    .pipe(gulp.dest('dist/views'))
 })
 
 /* IMAGES */
 gulp.task('minifyIcon', () => {
-    return gulp.src(['src/img/Medical-Sign-icon.png'])
-      .pipe(imagemin({
-        optimizationLevel: 3,
-        progressive:true,
-        interlaced: true
-      }))
-      .pipe(gulp.dest('dist/img'))
+  return gulp.src(['src/img/Medical-Sign-icon.png'])
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('dist/img'))
 })
 
 /* FONTS */
-gulp.task('fonts', function (){
+gulp.task('fonts', function () {
   return gulp.src([
-    'src/fonts/*'
-  ])
-  .pipe(gulp.dest('dist/fonts'))
+      'src/fonts/*'
+    ])
+    .pipe(gulp.dest('dist/fonts'))
 })
 
 /* LIBRARY */
 gulp.task('lib', ['transpile'], function () {
-    return gulp.src(lib)
-        .pipe(gulp.dest('dist/lib'))
+  return gulp.src(lib)
+    .pipe(gulp.dest('dist/lib'))
 })
 
 
